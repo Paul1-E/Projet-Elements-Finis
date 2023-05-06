e8b8e154dacc4b38b7549bca54c84559d4e72699
Paulin Eliat-Eliat <paulin.eliat-eliat@student.uclouvain.be>
2023-05-06 15:26:07 +0200

    Ajout des templates

---
+++ Project/src/main.c
@@ -0,0 +1,30 @@
+/*
+ *  main.c
+ *  Projet 2022-2023
+ *  Elasticite lineaire plane
+ *
+ *  Code de calcul
+ *
+ *  Copyright (C) 2023 UCL-IMMC : Vincent Legat
+ *  All rights reserved.
+ *
+ */
+ 
+#include "fem.h"
+
+int main(void)
+{  
+    femGeo* theGeometry = geoGetGeometry();   
+    geoMeshRead("../data/mesh.txt");
+    femProblem* theProblem = femElasticityRead(theGeometry,"../data/problem.txt");
+    femElasticityPrint(theProblem);
+    double *theSoluce = femElasticitySolve(theProblem); 
+    femNodes *theNodes = theGeometry->theNodes;
+    femFieldWrite(theNodes->nNodes,2,&theSoluce[0],"../data/U.txt");
+    femFieldWrite(theNodes->nNodes,2,&theSoluce[1],"../data/V.txt");
+    femElasticityFree(theProblem); 
+    geoFree();
+    return 0;  
+}
+
+ 
