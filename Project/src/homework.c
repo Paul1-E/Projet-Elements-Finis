d22d806fee62e8980b8832e33186e65fe7c68a1c
Paul1-E <114816635+Paul1-E@users.noreply.github.com>
2023-05-13 18:12:04 +0200

    Axisymetric problems

--- Project/src/homework.c
+++ Project/src/homework.c
@@ -103,6 +103,7 @@ double *femElasticitySolve(femProblem *theProblem)
     femGeo         *theGeometry = theProblem->geometry;
     femNodes       *theNodes = theGeometry->theNodes;
     femMesh        *theMesh = theGeometry->theElements;
+    int iCase = theProblem->planarStrainStress;
     
     int nLocal = theMesh->nLocalNode;
 
@@ -147,7 +148,30 @@ double *femElasticitySolve(femProblem *theProblem)
             
             for (i = 0; i < theSpace->n; i++) {    
                 dphidx[i] = (dphidxsi[i] * dydeta - dphideta[i] * dydxsi) / jac;       
-                dphidy[i] = (dphideta[i] * dxdxsi - dphidxsi[i] * dxdeta) / jac; }            
+                dphidy[i] = (dphideta[i] * dxdxsi - dphidxsi[i] * dxdeta) / jac; }
+            
+            //// Assemblage ////
+            // For axisym problems, we consider x --> r and y --> z
+            // Therefore, x > 0, and gravity is G = -g e_z
+            if (iCase == AXISYM) { 
+            for (i = 0; i < theSpace->n; i++) { 
+                for(j = 0; j < theSpace->n; j++) {
+                    A[mapX[i]][mapX[j]] += (dphidx[i] * a * dphidx[j] * x[iInteg] + 
+                                            dphidy[i] * c * dphidy[j] * x[iInteg] +
+                                            phi[i] * (b * dphidx[j] + a * phi[j]/x[iInteg] +
+                                            dphidx[i] * b * phi[j])) * jac * weight;                                                                                            
+                    A[mapX[i]][mapY[j]] += (dphidx[i] * b * dphidy[j] * x[iInteg] + 
+                                            dphidy[i] * c * dphidx[j] * x[iInteg] + 
+                                            phi[i] * b * dphidy[j]) * jac * weight;                                                                                           
+                    A[mapY[i]][mapX[j]] += (dphidy[i] * b * dphidx[j] * x[iInteg] + 
+                                            dphidx[i] * c * dphidy[j] * x[iInteg]) * jac * weight;                                                                                            
+                    A[mapY[i]][mapY[j]] += (dphidy[i] * a * dphidy[j] * x[iInteg] + 
+                                            dphidx[i] * c * dphidx[j] * x[iInteg]) * jac * weight; }}
+             for (i = 0; i < theSpace->n; i++) {
+                B[mapY[i]] -= phi[i] * x[iInteg] * g * rho * jac * weight; }
+            }
+
+            else {
             for (i = 0; i < theSpace->n; i++) { 
                 for(j = 0; j < theSpace->n; j++) {
                     A[mapX[i]][mapX[j]] += (dphidx[i] * a * dphidx[j] + 
@@ -159,8 +183,9 @@ double *femElasticitySolve(femProblem *theProblem)
                     A[mapY[i]][mapY[j]] += (dphidy[i] * a * dphidy[j] + 
                                             dphidx[i] * c * dphidx[j]) * jac * weight; }}
              for (i = 0; i < theSpace->n; i++) {
-                B[mapY[i]] -= phi[i] * g * rho * jac * weight; }}} 
+                B[mapY[i]] -= phi[i] * g * rho * jac * weight; }}}} 
   
+    
     int *theConstrainedNodes = theProblem->constrainedNodes;     
     for (int i=0; i < theSystem->size; i++) {
         if (theConstrainedNodes[i] != -1) {
